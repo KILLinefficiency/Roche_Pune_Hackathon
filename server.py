016041e092b548ce49d3f1fc24d81ef80009d3d4
KILLinefficiency <shreyas.sable2166@gmail.com>
2024-01-10 17:49:32 +0530

    major changes to code

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+from typing import List, Dict, Union
 from flask import Flask, request
+from stats import Stats
 from fizzbuzz import fizzbuzz
 
 server: Flask = Flask(__name__)
+stats: Stats = Stats()
 
 @server.route("/fizzbuzz")
-def read_data() -> list[str]:
+def read_data() -> List[str]:
     try:
         int1:  int = int(request.args.get("int1"))
         int2:  int = int(request.args.get("int2"))
@@ -14,13 +17,20 @@ def read_data() -> list[str]:
 
     str1: str = request.args.get("str1")
     str2: str = request.args.get("str2")
+    
+    record: List[Union[int, str]] = [int1, int2, limit, str1, str2]
+    stats.add_record(record)
 
-    return fizzbuzz(int1, int2, limit, str1, str2)
+    return fizzbuzz(*record)
 
-@server.route("/statistics")
-def show_stats() -> str:
-    pass
+@server.route("/stats")
+def show_stats() -> Dict[Union[str, any]]:
+    return {
+        "max": stats.get_max(),
+        "frequent": stats.get_frequent()
+    }
 
 
 if __name__ == "__main__":
     server.run()
+    stats.end()
--- stats.py
+++ stats.py
@@ -1,44 +1,43 @@
 import os
+from typing import List, Union
 import sqlite3 as sql
 
 class Stats:
     def __init__(self):
         self.db = "stats_db.db"
         if not os.path.exists(self.db):
-            self.connection = sql.connect(self.db)
+            self.connection = sql.connect(self.db, check_same_thread=False)
             self.cursor = self.connection.cursor()
-            self.cursor.execute("CREATE TABLE stats(int1 INT, int2 INT, lim INT, str1 TEXT, str2 TEXT, hits INT);")
+            self.cursor.execute("CREATE TABLE stats(int1 INT, int2 INT, lim INT, str1 TEXT, str2 TEXT, hits INT, stamp DATETIME);")
+            self.connection.commit()
         else:
-            self.connection = sql.connect(self.db)
+            self.connection = sql.connect(self.db, check_same_thread=False)
             self.cursor = self.connection.cursor()
 
-    def add_record(self, data: list[str]):
+    def add_record(self, data: List[Union[int, str]]):
         self.present_data = self.cursor.execute("SELECT * FROM stats WHERE int1 = ? AND int2 = ? AND lim = ? AND str1 = ? AND str2 = ?;", tuple(data))
         if len(self.present_data.fetchall()) == 0:
-            self.cursor.execute("INSERT INTO stats VALUES(?, ?, ?, ?, ?, 0)", tuple(data))
+            self.cursor.execute("INSERT INTO stats VALUES(?, ?, ?, ?, ?, 0, CURRENT_TIMESTAMP)", tuple(data))
             print(self.cursor.execute("SELECT * FROM stats;").fetchall())
         else:
-            self.cursor.execute("UPDATE stats SET hits = hits + 1 WHERE int1 = ? AND int2 = ? AND lim = ? AND str1 = ? AND str2 = ?;", tuple(data))
-            print(self.cursor.execute("SELECT * FROm stats;").fetchall())
+            self.cursor.execute("UPDATE stats SET hits = hits + 1, stamp = CURRENT_TIMESTAMP WHERE int1 = ? AND int2 = ? AND lim = ? AND str1 = ? AND str2 = ?;", tuple(data))
+            print(self.cursor.execute("SELECT * FROM stats;").fetchall())
+        self.connection.commit()
 
     def get_max(self):
         self.max_hits = self.cursor.execute("SELECT MAX(hits) FROM stats;").fetchone()[0]
-        self.params = self.cursor.execute("SELECT int1, int2, lim, str1, str2 FROM stats WHERE hits = ?", (self.max_hits,)).fetchone()
-        return {
-            "int1": self.params[0],
-            "int2": self.params[1],
-            "lim" : self.params[2],
-            "str1": self.params[3],
-            "str2": self.params[4]
-        }
+        self.params = self.cursor.execute("SELECT int1, int2, lim, str1, str2 FROM stats WHERE hits = ?;", (self.max_hits,)).fetchone()
+        self.fields = ["int1", "int2", "limit", "str1", "str2"]
+        if self.params == None:
+            self.params = [0] * 5
+        return dict(zip(self.fields, self.params))
+
+    def get_frequent(self):
+        self.recent = self.cursor.execute("SELECT MAX(stamp) FROM stats;").fetchone()[0]
+        self.recent_hits = self.cursor.execute("SELECT hits FROM stats WHERE stamp = ?;", (self.recent,)).fetchone()
+        if self.recent_hits == None:
+            self.recent_hits = [0]
+        return self.recent_hits[0]
 
     def end(self):
-        self.connection.commit()
         self.connection.close()
-
-s = Stats()
-s.add_record(["1", "1", "1", "d", "e"])
-s.add_record(["2", "2", "2", "d", "e"])
-s.add_record(["2", "2", "2", "d", "e"])
-print(s.get_max())
-s.end()
