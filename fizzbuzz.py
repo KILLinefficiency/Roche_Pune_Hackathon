459a7b088655aa1d42dc2b00ad244987154b692b
KILLinefficiency <shreyas.sable2166@gmail.com>
2024-01-10 15:08:58 +0530

    added initial fizzbuzz() function

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(x: int, y: int, limit: int, a: str, b: str) -> list[str]:
+    fb_data: list[str] = []
+
+    for n in range(1, limit + 1):
+        if n % (x * y) == 0:
+            fb_data.append(a + b)
+        elif n % x == 0:
+            fb_data.append(a)
+        elif n % y == 0:
+            fb_data.append(b)
+        else:
+            fb_data.append(str(n))
+
+    return fb_data
